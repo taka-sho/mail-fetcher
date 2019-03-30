155185544f30c1906190eb95d78b48f3dbc7ff18
taka-sho <takasho@keio.jp>
2019-03-31 03:47:00 +0900

    add price

--- api.js
+++ api.js
@@ -46,6 +46,57 @@ app.get('/api/v1/deposit/:email/:userName/:address/:date/:products', (req, res) 
         <td>${products}</td>
       </tr>
     </table>
+    <p>商品のご到着をお待ちくださいませ。</p>
+    </body>
+    `
+  }
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err){
+      console.log(err)
+      res.json({
+        message:`Failed sending mail to ${email}`
+      })
+    } else {
+      console.log('Message sent: ' + info.accepted)
+      res.json({
+        message:`Succeed sending mail to ${email}`
+      })
+    }
+  })
+})
+app.get('/api/v1/deposit-warning/:email/:userName/:address/:date/:products/:price', (req, res) => {
+  const { email, userName, address, date, products, price } = req.params
+  const mailOptions = {
+    to: email,
+    from: 'hakushin.express@gmail.com',
+    subject: 'ご入金のご確認',
+    html: `<body>
+    <p>${userName}様</p>
+    <p>更新綜合車両事務所です。</p>
+    <p>${userName}様がご注文された商品入金が確認できていません。ご注文内容以下の通りです。</p>
+    <table style='border-width: thin; border-style: solid'>
+      <tr>
+        <td>お客様名</td>
+        <td>${userName}</td>
+      </tr>
+      <tr>
+        <td>ご注文日時</td>
+        <td>${date}</td>
+      </tr>
+      <tr>
+        <td>ご住所</td>
+        <td>${address}</td>
+      </tr>
+      <tr>
+        <td>商品内容</td>
+        <td>${products}</td>
+      </tr>
+      <tr>
+        <td>商品代金</td>
+        <td>${price}円</td>
+      </tr>
+    </table>
+    <p>商品のご到着をお待ちくださいませ。</p>
     </body>
     `
   }
